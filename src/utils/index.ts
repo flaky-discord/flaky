8545eeecc5601a3e0cfa70b38740fa9245f70ac0
loldonut <loldonutph@gmail.com>
2024-04-21 14:34:27 +0800

    refactor(util): import directly from export instead

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,20 +3,9 @@ import countryList from './flagsCode';
 import loadCommands from './loadCommands';
 import loadEvents from './loadEvents';
 import registerCommands from './registerCommands';
-import {
-    defineUVIndex,
-    isDevMode,
-    getFromConfig,
-    getRequest,
-    logger,
-} from './util';
 
+export * from './util';
 export {
-    defineUVIndex,
-    isDevMode,
-    getFromConfig,
-    getRequest,
-    logger,
     UserSlashCommandBuilder,
     countryList,
     loadCommands,
